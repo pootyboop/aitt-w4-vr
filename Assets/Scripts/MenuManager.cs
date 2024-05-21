264637849352a5f117c1b49a58f01fcc54528ccc
Elliot <elliotgmann@gmail.com>
2024-05-21 19:52:00 +0100

    Font, menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,38 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class MenuManager : MonoBehaviour
 {
     public static MenuManager instance;
     bool inMenu = true;
 
-    Vector3 playerSpawnpoint;
+    Vector3 playerSpawnpoint = Vector3.zero;
     Quaternion playerSpawnRot;
 
-    public GameObject menuPlayer, gamePlayer;
+    public GameObject menuPlayer, gamePlayer, menu;
+
+    public SteamVR_Action_Single startLAction, startRAction;
 
     private void Start()
     {
         instance = this;
+        SetInMenu(inMenu);
+    }
+
+    private void Update()
+    {
+        if (inMenu && startLAction.axis > 0.9f && startRAction.axis > 0.9f)
+        {
+            SetInMenu(false);
+        }
     }
 
     public void SetInMenu(bool newInMenu)
     {
         inMenu = newInMenu;
-        menuPlayer.SetActive(inMenu);
-        gamePlayer.SetActive(!inMenu);
+        menu.SetActive(inMenu);
 
-        if (!inMenu && playerSpawnpoint == null)
+        if (inMenu)
         {
-            playerSpawnpoint = gamePlayer.transform.position;
-            playerSpawnRot = gamePlayer.transform.rotation;
+            menuPlayer.SetActive(true);
+            gamePlayer.SetActive(false);
         }
 
         else
         {
-            gamePlayer.transform.position = playerSpawnpoint;
-            gamePlayer.transform.rotation = playerSpawnRot;
+            menuPlayer.SetActive(false);
+            gamePlayer.SetActive(true);
+
+            if (playerSpawnpoint == Vector3.zero)
+            {
+                playerSpawnpoint = gamePlayer.transform.position;
+                playerSpawnRot = gamePlayer.transform.rotation;
+            }
+
+            else
+            {
+                gamePlayer.transform.position = playerSpawnpoint;
+                gamePlayer.transform.rotation = playerSpawnRot;
+            }
         }
     }
 }
